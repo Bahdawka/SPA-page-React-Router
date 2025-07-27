adff1784fafba241f587aaaa56542476c36f5af0
Bahdawka laptop <it.horobets@gmail.com>
2025-07-27 22:36:55 +0200

    Refactored routing to use createBrowserRouter and RouterProvider with layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
 import Menu from './components/Menu'
 import FooterLinks from './components/FooterLinks'
+import type { ReactElement } from 'react'
+
+const withLayout = (Component: () => ReactElement) => (
+  <>
+    <Menu />
+    <Component />
+    <FooterLinks />
+  </>
+)
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: withLayout(Home)
+},
+{
+  path: "/about",
+  element: withLayout(About)
+},
+{
+  path: "/contact",
+  element: withLayout(Contact)
+},
+{
+  path: "*",
+  element: withLayout(NotFound)
+}
+])
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <FooterLinks />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
 export default App
+
+
+// const App = () => {
+//   return (
+// <BrowserRouter> <-- варіант вкладеної маршрутизаціЇ через BrowserRouter з Routes та Route
+//   <Menu />           імпортуємо BrowserRouter,Routes,Route
+//   <Routes>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/about" element={<About />} />
+//     <Route path="/contact" element={<Contact />} />
+//     <Route path="*" element={<NotFound />} />
+//   </Routes>
+//   <FooterLinks />
+// </BrowserRouter>
+// )
+// }
